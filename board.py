2d37d7233ad13ed9d7e5ce8175e955dbf3c6db21
Jeff Shum <jeffshum1@gmail.com>
2020-08-03 16:19:26 +0700

    Add a render function which prints a given board state

--- board.py
+++ board.py
@@ -2,15 +2,15 @@
 import random
 
 def dead_state(width, height):
-    board = []
+    board_state = []
     for n in range(height):
         row = [0 for x in range(width)]
-        board.append(row)
-    return board    
+        board_state.append(row)
+    return board_state    
 
 def random_state(width, height):
-    state = dead_state(width, height)
-    for row in state:
+    board_state = dead_state(width, height)
+    for row in board_state:
         for cell_num, cel in enumerate(row):
             random_number = random.random()
             if random_number >= 0.5:
@@ -18,7 +18,19 @@ def random_state(width, height):
             else:
                 cell_state = 0 
             row[cell_num] = cell_state
-    return state
+    return board_state
+
+def render(board_state):
+    print('-' * (len(board_state[0]) + 2))
+    for row in board_state:
+        print('|', end ='')
+        for cell in row:
+            if cell == 1:
+                print('#', end='')
+            else:
+                print(' ', end='')
+        print('|')        
+    print('-' * (len(board_state[0]) + 2))
 
 def main(argv):
     return print(random_state(int(argv[1]),int(argv[2])))
